3b5621807f7052d8b79dd390a0aaa803b269939e
Scott A Eckenthal <scott.eckenthal@gmail.com>
2015-12-28 19:53:49 -0500

    Re-indenting

--- src/utils/process-markdown.js
+++ src/utils/process-markdown.js
@@ -1,16 +1,40 @@
 'use strict';
 
 var marked = require('marked'),
-	renderer = new marked.Renderer();
+renderer = new marked.Renderer();
 
 renderer.heading = function(text, level, raw) {
-  return '<h'
-    + level
-    + '>'
-    + text
-    + '</h'
-    + level
-    + '>\n';
+	return '<h'
+	+ level
+	+ '>'
+	+ text
+	+ '</h'
+	+ level
+	+ '>\n';
+};
+
+renderer.code = function(code, lang, escaped) {
+
+	if (this.options.highlight) {
+		var out = this.options.highlight(code, lang);
+		if (out != null && out !== code) {
+			escaped = true;
+			code = out;
+		}
+	}
+
+	if (!lang) {
+		return '<pre><code>'
+		+ (escaped ? code : escape(code, true))
+		+ '\n</code></pre>';
+	}
+
+	return '<pre><code class="'
+	+ this.options.langPrefix
+	+ escape(lang, true)
+	+ '">'
+	+ (escaped ? code : escape(code, true))
+	+ '\n</code></pre>\n';
 };
 
 function processMarkdown(markdownString) {
