ae42c5abc3b8edf7866f3f44fa01d1dc4a136266
Scott A Eckenthal <scott.eckenthal@gmail.com>
2016-01-27 08:16:14 -0500

    Uses file processor to parse glob

--- src/node-hologram.js
+++ src/node-hologram.js
@@ -1,7 +1,12 @@
 'use strict';
 
 var _ = require('lodash'),
-	glob = require('glob');
+	glob = require('glob'),
+	rfr = require('rfr');
+
+var processFile = rfr('src/utils/process-file');
+
+var options = {};
 
 function NodeHologram(src, ops) {
 
@@ -9,7 +14,19 @@ function NodeHologram(src, ops) {
 		throw Error('NodeHologram must be instantiated with arg `src`.');
 	}
 
-	this.files = glob.sync(src);
+	this.src = src;
+}
+
+NodeHologram.prototype.parse = function() {
+
+	var files = glob.sync(this.src),
+		result = [];
+
+	_.each(files, function(fileString) {
+		result.push(processFile(fileString));
+	});
+
+	return result;
 }
 
 module.exports = NodeHologram;
--- test/src/node-hologram._SPEC.js
+++ test/src/node-hologram._SPEC.js
@@ -3,18 +3,27 @@
 var expect = require('chai').expect,
 	rfr = require('rfr');
 
-var NodeHologram = rfr('src/node-hologram');
+var NodeHologram = rfr('src/node-hologram'),
+	test = rfr('test/utils/test');
 
-describe('NodeHologram()', function() {
-
-	it('It globs source upon instantiation.', function() {
-		var good = new NodeHologram('../in/*.scss');
-		expect(good.files).to.be.ok;
-	});
+describe('NodeHologram', function() {
 
 	it('It throws an error when incorrect args are provided.', function() {
 		expect(function() {
 			var bad = new NodeHologram();
 		}).to.throw();
+	});
+
+	describe('.parse()', function() {
+
+		it('It parses source as expected.', function() {
+
+			var nh = new NodeHologram('test/in/multiple/**/*.scss');
+
+			var result = nh.parse(),
+				expected = rfr('test/expected/multiple.json');
+
+			test(result, expected);
+		});
 	});
 });
